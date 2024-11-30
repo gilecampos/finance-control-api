00741d870784cac70e9ecf318e80c6f2175c3579
Gileandro Campos <gileandrocampos8@gmail.com>
2024-11-30 10:15:50 -0500

    feat: adicionar validações para os novos campos adicionado a API

--- app/validators/user.ts
+++ app/validators/user.ts
@@ -4,6 +4,8 @@ export const createUserValidator = vine.compile(
   vine.object({
     full_name: vine.string().trim(),
     email: vine.string().email(),
-    password: vine.string().minLength(8).maxLength(32).confirmed()
+    password: vine.string().minLength(8).maxLength(32).confirmed(),
+    mobile_phone: vine.string().minLength(10).maxLength(11),
+    email_is_confirmed: vine.boolean().optional()
   })
 )
